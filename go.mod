4a158ea1010cc4558b5ae4b22e038e450c5c6a7d
matheusmosca <matheusmoscag@gmail.com>
2020-12-16 21:48:25 -0300

    add create and show endpoints

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/matheusmosca/BDProject
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-redis/redis/v8 v8.4.2
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
+
+var ctx = context.Background()
+
+func newRedisConnection() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	return rdb
+}
 
 type Book struct {
 	Name       string `json:"name"`
@@ -14,29 +27,97 @@ type Book struct {
 	TotalPages int    `json:"totalpages"`
 }
 
-var books = []Book{
-	{
-		Name:       "And then there were none",
-		Author:     "Agatha Christie",
-		TotalPages: 272,
-	},
-	{
-		Name:       "Murder on the Orient Express",
-		Author:     "Agatha Christie",
-		TotalPages: 256,
-	},
+//* Repository
+
+func getBook(bookName string) (*Book, error) {
+	rdb := newRedisConnection()
+	defer rdb.Close()
+
+	bookJSON, err := rdb.Get(ctx, bookName).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	rawBook := &Book{}
+	err = json.Unmarshal([]byte(bookJSON), rawBook)
+	if err != nil {
+		return nil, err
+	}
+
+	return rawBook, nil
 }
 
-func showBooks(w http.ResponseWriter, r *http.Request) {
+func storeBook(book *Book) error {
+	rdb := newRedisConnection()
+	defer rdb.Close()
+
+	_, err := getBook(book.Name)
+	if err == nil {
+		return errors.New("This key name already exists on the database")
+	}
+
+	bookJSON, err := json.Marshal(book)
+
+	if err != nil {
+		return err
+	}
+
+	if err = rdb.Set(ctx, book.Name, bookJSON, 0).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//* Handlers
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "Application/json")
+	newBook := &Book{}
+
+	err := json.NewDecoder(r.Body).Decode(newBook)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
+	err = storeBook(newBook)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	json.NewEncoder(w).Encode(newBook)
+}
+
+func showBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "Application/json")
 
-	json.NewEncoder(w).Encode(&books)
+	params := mux.Vars(r)
+
+	book, err := getBook(params["name"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/books", showBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{name}", showBook).Methods("GET")
 
 	fmt.Println("Starting server in port 3030...")
 	http.ListenAndServe(":3030", r)
